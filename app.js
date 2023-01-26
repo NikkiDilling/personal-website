f67baa61b8c0de81985e38d318a1187af3b38b1e
Nikki Dilling <ndillinghansen@gmail.com>
2023-01-26 11:55:11 +0900

    finished coding icons js pop-up feature

--- app.js
+++ app.js
@@ -5,39 +5,49 @@ let progressBar = document.querySelector('.progress-bar');
 let progessNum = document.querySelector('.progress-num');
 
 const iconsDiv = document.querySelector('.article-icons');
- 
-let active = false;
 
 //Functions
 function updateProgressBar(){
+    //updating scroll bar text and progress bar
     progressBar.style.height = `${getScrollPercentage()}%`;
     progessNum.innerText = `${Math.ceil(getScrollPercentage())}%`
 
     //stopping the progress bar at 100%
-    
         if(getScrollPercentage() >= 100){
             progressBar.style.height = `100%`;
             progessNum.innerText = `100%`;
         }
 
-
+    //look up this function
     requestAnimationFrame(updateProgressBar);
 }
 
 function getScrollPercentage(){
+    //returning the position of the user on the screen
     return (window.scrollY / (document.body.scrollHeight - window.innerHeight) * 100);
 }
 
 
 iconsDiv.addEventListener('click', function(e){
 
-    //console.log(e.target.getAttribute('id'));
-
     if(e.target.tagName.toLowerCase() === 'img'){
-        e.target.parentElement.classList.toggle('active');
-        e.target.nextElementSibling.classList.toggle('activeP');
-        e.target.nextElementSibling.innerText = e.target.getAttribute('id');
+      
+        //adds an active tag, to show the name of the icon
+        if(e.target.parentElement.classList.contains('active') == false){
 
+            e.target.parentElement.classList.add('active');
+            //setting p's inner text
+            e.target.nextElementSibling.innerText = e.target.getAttribute('id');
+            //console.log("added");
+
+        //Removes the active tag and therefore also the name of the icon
+        }else if(e.target.parentElement.classList.contains('active') == true){
+
+            //console.log("removed");
+            e.target.parentElement.classList.remove('active');
+            //removing p's inner text            
+            e.target.nextElementSibling.innerText = "";
+        }
     }
 
    
@@ -45,5 +55,7 @@ iconsDiv.addEventListener('click', function(e){
 
 
 
+
 //Running Functions
 updateProgressBar();
+
